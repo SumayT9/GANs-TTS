04c184bcf4457dbbdd3bebd85ac71e9a534163da
Kevin <kwoodman22@priorypanther.com>
2020-12-12 18:29:15 -0800

    Added all files need to run [preprocess.py]
    This also added instructions to the README.md

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # GANs-TTS
 GANs project for inspirit
+
+**MAKE SURE YOU ARE USING PYTHON 3.7**
+
+Steps to use:
+
+1 Download Real-Time-Voice-Cloning [RTVC] from github.
+
+2 Switch encoder_preprocess.py & preprocess.py in RTVC with the given replacements
+
+3 Put saved_models into RTVC/encoder
+
+4 Put the temp folder in the location you will run Youtube.py
+        This is because python has trouble creating .webm files
+
+5 Run Youtube.py
+
+6 run pip3 install -r RTVC/requirments.txt
+
+7 Run encoder_preprocess.py  Path/to/out/Youtube_dataset
+
+8 Run encoder_train.py pretrained Path/to/out/Youtube_dataset/SV2TTS/encoder
--- Youtube.py
+++ Youtube.py
@@ -5,19 +5,20 @@ import sys, getopt
 def parse(argv):
     length = 3
     url = ""
+    urlArray = {}
     title = ""
     search = "baseball commentary"
-    max = 2
+    max = 5
     speaker_num = 1
     
     try:
-      opts, args = getopt.getopt(argv,"hl:u:t:s:m:S:",["length=","url=","title=","search=","max_results=","speaker="])
+      opts, args = getopt.getopt(argv,"hl:u:t:q:m:s:",["length=","url=","title=","search=","max_results=","speaker="])
     except getopt.GetoptError:
-      print ('Youtube.py -l <length> -u <url> -t <title> -s <search> -m <max_results> -S <speaker number>')
+      print ('Youtube.py -l <length> -u <url> -t <title> -q <query> -m <max_results> -s <speaker number>')
       sys.exit(2)
     for opt, arg in opts:
        if opt == '-h':
-          print ('Youtube.py -l <length> -u <url> -t <title> -s <search> -m <max_results> -S <speaker number>')
+          print ('Youtube.py -l <length> -u <url> -t <title> -q <query> -m <max_results> -s <speaker number>')
           sys.exit()
        elif opt in ("-l", "--length"):
           length = arg
@@ -25,11 +26,11 @@ def parse(argv):
           url = arg
        elif opt in ("-t", "--title"):
           title = arg
-       elif opt in ("-s", "--search"):
+       elif opt in ("-q", "--search"):
           search = arg
        elif opt in ("-m", "--max_results"):
           max = arg
-       elif opt in ("-S", "--speaker"):
+       elif opt in ("-s", "--speaker"):
           speaker_num = arg
        return length,url,title,search,max,speaker_num
 
