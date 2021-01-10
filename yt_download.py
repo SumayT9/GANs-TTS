ef5d8548bba8e3f097a015eacf72488a58053430
Kevin <kwoodman22@priorypanther.com>
2021-01-09 20:30:37 -0800

    Changed yt_download.py to cut around silence
    The code for cutting works however I was unable to test it's integration into yt_download due to not having a properly formatted URL file.

--- yt_download.py
+++ yt_download.py
@@ -28,31 +28,68 @@ for filename in os.listdir('URLs'):
     #download videos
     for url in links:
         
-        # print("getting video...", end="")
         os.system("youtube-dl --no-check-certificate -f bestaudio -o \"temp/audio.%(ext)s\" \""+url+"\"")
-        #   filename = "temp/" + os.listdir("temp")[0]
-        #   print("finished downloading (", filename, ")", sep="")
-        #remove illegal characters
         for c in "\"\' |&?!()+-*/":
             title = title.replace(c, "")
-
-        # print("converting to wav...", end="")
-        #os.system("ffmpeg -i temp/"+title+".webm -c:a pcm_f32le input/"+title+".wav")
-        #pydub.AudioSegment.from_file(filename).export("input/"+title+".wav", format="wav")
-        # print("finished converting (input/"+title+".webm)")
         ytSuccesses += 1
 
-        # print("splitting...", end="")
-        # fileDir = dir + "/" + title + ".wav"
-        # subprocess.check_output("ffmpeg -loglevel warning -i "+fileDir+" -ar 16000 -sample_fmt s16 -ac 1 -f segment -segment_time "+str(lengthOfClip)+" -vn -c copy out/"+title+"_%03d.wav", shell=True)
-        #command = ['ffmpeg', '-i', fileDir, '-f', 'segment', '-segment_time', '3', 'out/'+title+'%09d.wav']
-        #subprocess.run(command,stdout=subprocess.PIPE,stdin=subprocess.PIPE)
-        os.system("ffmpeg -loglevel warning -i temp/audio.webm -ar 16000 -sample_fmt s16 -ac 1 -f segment -segment_time "+str(lengthOfClip)+" -vn out/Youtube_dataset/"+dir+"/"+title+"_%03d.wav")
-        # print("finished")
-        # print()
-    
-    #os.remove(filename)
-
+        os.system("ffmpeg -loglevel warning -i temp/audio.webm -ar 16000 -sample_fmt s16 -ac 1 -c copy temp/" + title + ".wav") #saves as .wav
+        
+        
+        file = "temp/" + title + ".wav"
+        os.system("ffmpeg -i " + file + " -af silencedetect=noise=-30dB:d=0.3 -f null - 2> vol.txt") #CHANGE SILENCE TIME AT: d=
+        with open('vol.txt', 'r+') as temp:
+            text = temp.read()
+            text = text.replace('silence_start: ','split')
+            text = text.replace('silence_end: ','split')
+            text = text.replace('| silence_duration: ','split')
+            text = text.replace('\n[silencedetect @','split')
+            text = text.replace('\nsize','split')
+            text = text.split('split')
+            text = text[1:]
+            print("Trimming")
+            i = 0
+            while i < len(text):
+                if "x" in text[i]: #0x564b3cb09040 acts strange as a srting idk why
+                    text.remove(text[i])
+                elif "-" in text[i]:
+                    text[i] = "0.0"
+                    i += 1
+                else:
+                    i += 1
+            print(text)
+            i = 1
+            clip = 0
+            try:
+                if text[0] == 0.0:
+                    time = text[1]
+                else:
+                    time = 0.0
+                    temp = float(text[0])
+                    os.system("ffmpeg -loglevel warning -ss " + str(time) + " -i " + file + " -t " + str(temp) + " -c copy out/Youtube_dataset/" + dir + file[6:len(file)-4] + "_" + str(clip) + ".wav")
+                    time = text[1]
+                    clip += 1
+            except:
+                print("no silence")
+                os.system("ffmpeg -loglevel warning -i " + file + " -c copy out/Youtube_dataset/" + dir + file[6:len(file)-4] + "_" + str(clip) + ".wav")
+            while i < len(text):
+                j = 2
+                temp = float(text[i+j]) - float(time)
+                #Un-comment to add min time (Not fully tested)
+                    #while (temp < 3) & (i+j < len(text)): #CHANGE MIN TIME HERE
+                    #j += 2
+                    #temp = float(text[i+j]) - float(time)
+                os.system("ffmpeg -loglevel warning -ss " + str(time) + " -i " + file + " -t " + str(temp) + " -c copy out/Youtube_dataset/" + dir + file[6:len(file)-4] + "_" + str(clip) + ".wav")
+                i += 3
+                try:
+                    time = text[i]
+                except:
+                    print("exit loop")
+                clip += 1
+                print("Cut")
+            print("Done!")
+        
+        
 print(ytSuccesses, "Youtube videos successfully downloaded")
 
 
