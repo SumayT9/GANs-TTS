fd7b309d2483f4e32086e5cfcb0c32a773bc354f
Laksh Bhambhani <lakshbh@gmail.com>
2021-02-06 22:52:59 -0800

    added column reading capability

--- cv_textreader/cv_textreader.py
+++ cv_textreader/cv_textreader.py
@@ -1,65 +1,117 @@
-# Import required packages 
-import cv2 
-import pytesseract 
+# # Import required packages 
+# import cv2 
+# import pytesseract 
 
-# Mention the installed location of Tesseract-OCR in your system 
-pytesseract.pytesseract.tesseract_cmd = R'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+# # Mention the installed location of Tesseract-OCR in your system 
+# pytesseract.pytesseract.tesseract_cmd = R'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 
-# Read image from which text needs to be extracted 
-img = cv2.imread("picture.jpg") 
+# # Read image from which text needs to be extracted 
+# img = cv2.imread("picture.jpg") 
 
-# Preprocessing the image starts 
+# # Preprocessing the image starts 
 
-# Convert the image to gray scale 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+# # Convert the image to gray scale 
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 
-# Performing OTSU threshold 
-ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
+# # Performing OTSU threshold 
+# ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
 
-# Specify structure shape and kernel size. 
-# Kernel size increases or decreases the area 
-# of the rectangle to be detected. 
-# A smaller value like (10, 10) will detect 
-# each word instead of a sentence. 
-rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18)) 
+# # Specify structure shape and kernel size. 
+# # Kernel size increases or decreases the area 
+# # of the rectangle to be detected. 
+# # A smaller value like (10, 10) will detect 
+# # each word instead of a sentence. 
+# rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18)) 
 
-# Appplying dilation on the threshold image 
-dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1) 
+# # Appplying dilation on the threshold image 
+# dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1) 
 
-# Finding contours 
-contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, 
-												cv2.CHAIN_APPROX_NONE) 
+# # Finding contours 
+# contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, 
+# 												cv2.CHAIN_APPROX_NONE) 
 
-# Creating a copy of image 
-im2 = img.copy() 
+# # Creating a copy of image 
+# im2 = img.copy() 
 
-# A text file is created and flushed 
-file = open("recognized.txt", "w+") 
-file.write("") 
-file.close() 
+# # A text file is created and flushed 
+# file = open("recognized.txt", "w+") 
+# file.write("") 
+# file.close() 
 
-# Looping through the identified contours 
-# Then rectangular part is cropped and passed on 
-# to pytesseract for extracting text from it 
-# Extracted text is then written into the text file 
-for cnt in contours: 
-	x, y, w, h = cv2.boundingRect(cnt) 
+# # Looping through the identified contours 
+# # Then rectangular part is cropped and passed on 
+# # to pytesseract for extracting text from it 
+# # Extracted text is then written into the text file 
+# for cnt in contours: 
+# 	x, y, w, h = cv2.boundingRect(cnt) 
 	
-	# Drawing a rectangle on copied image 
-	rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2) 
+# 	# Drawing a rectangle on copied image 
+# 	rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2) 
 	
-	# Cropping the text block for giving input to OCR 
-	cropped = im2[y:y + h, x:x + w] 
+# 	# Cropping the text block for giving input to OCR 
+# 	cropped = im2[y:y + h, x:x + w] 
 	
-	# Open the file in append mode 
-	file = open("recognized.txt", "a") 
+# 	# Open the file in append mode 
+# 	file = open("recognized.txt", "a") 
 	
-	# Apply OCR on the cropped image 
-	text = pytesseract.image_to_string(cropped) 
+# 	# Apply OCR on the cropped image 
+# 	text = pytesseract.image_to_string(cropped) 
 	
-	# Appending the text into file 
-	file.write(text) 
-	file.write("\n") 
+# 	# Appending the text into file 
+# 	file.write(text) 
+# 	file.write("\n") 
 	
-	# Close the file 
-	file.close 
+# 	# Close the file 
+# 	file.close 
+
+
+import cv2
+import sys
+
+SCALE = 4
+AREA_THRESHOLD = 427505.0 / 2
+
+def show_scaled(name, img):
+    try:
+        h, w  = img.shape
+    except ValueError:
+        h, w, _  = img.shape
+    cv2.imshow(name, cv2.resize(img, (w // SCALE, h // SCALE)))
+
+def main():
+    img = cv2.imread("CSzyw.png")
+    print("Hello World")
+    img = img[10:-10, 10:-10] # remove the border, it confuses contour detection
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # show_scaled("original", gray)
+
+    # black and white, and inverted, because
+    # white pixels are treated as objects in
+    # contour detection
+    thresholded = cv2.adaptiveThreshold(
+                gray, 255,
+                cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV,
+                25,
+                15
+            )
+    # show_scaled('thresholded', thresholded)
+    # I use a kernel that is wide enough to connect characters
+    # but not text blocks, and tall enough to connect lines.
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (13, 33))
+    closing = cv2.morphologyEx(thresholded, cv2.MORPH_CLOSE, kernel)
+
+    contours, hierarchy = cv2.findContours(closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # show_scaled("closing", closing)
+
+    for contour in contours:
+        convex_contour = cv2.convexHull(contour)
+        area = cv2.contourArea(convex_contour)
+        if area > AREA_THRESHOLD:
+            cv2.drawContours(img, [convex_contour], -1, (255,0,0), 3)
+
+    # show_scaled("contours", img)
+    cv2.imwrite("contours.png", img)
+    cv2.waitKey()
+
+if __name__ == '__main__':
+    main()
