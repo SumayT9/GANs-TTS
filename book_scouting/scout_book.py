74a551696b8055aeab9a4668e45836db256818bd
Laksh Bhambhani <lakshbh@gmail.com>
2021-05-29 23:13:13 -0700

    pushed app version

--- book_scouting/scout_book.py
+++ book_scouting/scout_book.py
@@ -1,7 +1,7 @@
 from googlesearch import search
 
-transcribed_text = "cat in the hat"
+transcribed_text = "hello"
 
-results = search((transcribed_text + " pdf free filetype:pdf"), lang = "en")
+results = search((transcribed_text + " pdf free filetype:pdf"), lang = "en", num_results=5)
 
-print(results)
+print(str(results))
