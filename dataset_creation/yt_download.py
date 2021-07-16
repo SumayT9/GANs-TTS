b41210c23e686fb3a5ae094d161ce0592883a6fe
Kevin Woodman <kwoodman22@priorypanther.com>
2021-07-15 20:43:40 -0700

    Update yt_download.py

--- dataset_creation/yt_download.py
+++ dataset_creation/yt_download.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import json
+
 from pydub import AudioSegment
 
 try:
@@ -9,31 +10,29 @@ try:
 except:
     os.mkdir("download_output")
 
-def download_audio(url):
+def download_audio(url,speaker):
 
-    os.system("youtube-dl --no-check-certificate -f bestaudio -o \"download_output/audio_temp.%(ext)s\" \""+url+"\"")
+    print("")
+    print("")
+    print("")
+    print("SPEAKER: " + speaker)
+    print("URL: " + url)
+    print("AAAAAAA: " + "youtube-dl --no-check-certificate -f bestaudio -o \"download_output/" + speaker + ".webm\" \""+url+"\"")
+    print("")
+    print("")
+    print("")
+    
+    os.system("youtube-dl --no-check-certificate -f bestaudio -o \"download_output/" + speaker + ".webm\" \""+url+"\"")
 
     
     audio = None
-    try:
 
-        # Save as m4a
-        audio = AudioSegment.from_file("download_output/audio_temp.m4a")
-        # delete the m4a
-        os.remove("download_output/audio_temp.m4a")
-    except:
-        # Convert webm to wav
-        os.system("ffmpeg -loglevel warning -i download_output/audio_temp.webm -ar 16000 -sample_fmt s16 -ac 1 -vn download_output/" + "audio_temp" + ".wav") #saves as .wav
-        
-        # Save wav as audiosegment
-        audio = AudioSegment.from_file("download_output/audio_temp.wav")
-        
-        # Delete webm
-        os.remove("download_output/audio_temp.webm")
-    
-        # Delete wav
-        os.remove("download_output/audio_temp.wav")
-    
+    # Saveas m4a
+    audio = AudioSegment.from_file("download_output/" + speaker + ".webm")
+                    
+    # delete the m4a
+    os.remove("download_output/" + speaker + ".webm")
+                
     return audio
     
 
