49dab5ab8c71acf964be31d9901c2b1f8ef55220
SumayT9 <63134346+SumayT9@users.noreply.github.com>
2021-07-17 00:00:49 -0700

    Update cv_textreader.py

--- cv_textreader/cv_textreader.py
+++ cv_textreader/cv_textreader.py
@@ -73,72 +73,73 @@ import pytesseract
 SCALE = 4
 AREA_THRESHOLD = 427505.0 / 2
 
+class CVTextreader():
+    def show_scaled(self, name, img):
+        try:
+            h, w = img.shape
+        except ValueError:
+            h, w, _ = img.shape
+        cv2.imshow(name, cv2.resize(img, (w // SCALE, h // SCALE)))
 
-def show_scaled(name, img):
-    try:
-        h, w = img.shape
-    except ValueError:
-        h, w, _ = img.shape
-    cv2.imshow(name, cv2.resize(img, (w // SCALE, h // SCALE)))
 
+    def main(self, img_path):
+        print("shape")
+        img = cv2.imread(img_path)
+        print(img.shape)
+        img = img[10:-10, 10:-10]  # remove the border, it confuses contour detection
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # show_scaled("original", gray)
 
-def main():
-    img = cv2.imread("data/gov_1.jpeg")
-    print("shape")
-    print(img.shape)
-    img = img[10:-10, 10:-10]  # remove the border, it confuses contour detection
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # show_scaled("original", gray)
+        # black and white, and inverted, because
+        # white pixels are treated as objects in
+        # contour detection
+        thresholded = cv2.adaptiveThreshold(
+            gray, 255,
+            cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV,
+            25,
+            15
+        )
+        # show_scaled('thresholded', thresholded)
+        # I use a kernel that is wide enough to connect characters
+        # but not text blocks, and tall enough to connect lines.
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (13, 33))
+        closing = cv2.morphologyEx(thresholded, cv2.MORPH_CLOSE, kernel)
 
-    # black and white, and inverted, because
-    # white pixels are treated as objects in
-    # contour detection
-    thresholded = cv2.adaptiveThreshold(
-        gray, 255,
-        cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV,
-        25,
-        15
-    )
-    # show_scaled('thresholded', thresholded)
-    # I use a kernel that is wide enough to connect characters
-    # but not text blocks, and tall enough to connect lines.
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (13, 33))
-    closing = cv2.morphologyEx(thresholded, cv2.MORPH_CLOSE, kernel)
+        contours, hierarchy = cv2.findContours(closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # show_scaled("closing", closing)
 
-    contours, hierarchy = cv2.findContours(closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # show_scaled("closing", closing)
+        for contour in contours:
+            print('contour')
+            convex_contour = cv2.convexHull(contour)
+            area = cv2.contourArea(convex_contour)
+            if area > AREA_THRESHOLD:
+                #cv2.drawContours(img, [convex_contour], -1, (255, 0, 0), 3) drawing the contours screws recognition up
+                x, y, w, h = cv2.boundingRect(contour)
 
-    for contour in contours:
-        print('contour')
-        convex_contour = cv2.convexHull(contour)
-        area = cv2.contourArea(convex_contour)
-        if area > AREA_THRESHOLD:
-            #cv2.drawContours(img, [convex_contour], -1, (255, 0, 0), 3) drawing the contours screws recognition up
-            x, y, w, h = cv2.boundingRect(contour)
+                thresholded = 255 - thresholded
 
-            thresholded = 255 - thresholded
+                # 	# Drawing a rectangle on copied image
+                rect = cv2.rectangle(img, (x-10, y-10), (x + w + 50, y + h + 50), (0, 255, 0), 2)
 
-            # 	# Drawing a rectangle on copied image
-            rect = cv2.rectangle(img, (x-10, y-10), (x + w + 50, y + h + 50), (0, 255, 0), 2)
+                # 	# Cropping the text block for giving input to OCR
+                cropped = img[y:y + h + 50, (x-10):x + w + 50]
 
-            # 	# Cropping the text block for giving input to OCR
-            cropped = img[y:y + h + 50, (x-10):x + w + 50]
-        
-            file = open("recognized.txt", "a")
+                file = open("recognized.txt", "a")
 
-            # Apply OCR on the cropped image
-            text = pytesseract.image_to_string(cropped)
+                # Apply OCR on the cropped image
+                text = pytesseract.image_to_string(cropped)
 
-            # Appending the text into file
-            file.write(text)
-            file.write("\n")
+                # Appending the text into file
+                file.write(text)
+                file.write("\n")
 
-            # Close the file
-            file.close
+                # Close the file
+                file.close
 
-            # show_scaled("contours", img)
-    cv2.imwrite("contours.png", img)
+                # show_scaled("contours", img)
+        cv2.imwrite("contours.png", img)
 
 
 if __name__ == '__main__':
-    main()
+    textreader = CVTextreader()
+    textreader.main("data/gov_1.jpeg")
